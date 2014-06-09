16b7996b4c61bb036ddf4b9d6cf54972dbde2624
Jose Contreras <jocontre@joselcontreras.com>
2014-06-08 20:00:41 -0700

    Sockets

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -4,7 +4,7 @@ function PollListCtrl($scope, Poll) {
 }
 
 // Controller for an individual poll
-//Using sockets bel
+//Using sockets belo
 function PollItemCtrl($scope, $routeParams, socket, Poll) {	
 	$scope.poll = Poll.get({pollId: $routeParams.pollId});
 	
