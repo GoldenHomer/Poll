7952086d55f16d57c5e4d204d692224c3eb20ca8
Jose Contreras <jocontre@joselcontreras.com>
2014-08-21 22:25:06 -0700

    Took out unnecessary part

--- routes/index.js
+++ routes/index.js
@@ -47,7 +47,7 @@ exports.poll = function(req, res) {
 					var vote = choice.votes[v];
 					totalVotes++;
 
-					if(vote.ip === (req.header('x-forwarded-for') || req.ip)) {
+					if(vote.ip === req.ip) {
 						userVoted = true;
 						userChoice = { _id: choice._id, text: choice.text };
 					}
